18f10729e8de0951efa4eb2cd1e18c0d4c8f13fc
andaluciacurtis <143487070+andaluciacurtis@users.noreply.github.com>
2023-11-28 22:53:12 +0000

    Added correct bars + hover amounts

--- script.js
+++ script.js
@@ -2,30 +2,46 @@ const response = await fetch('./data.json');
 const data = await response.json();
 
 const chart = document.querySelector(".chart");
-const balance = document.querySelector(".balance");
 const totalAmount = document.querySelector(".total");
-const increase = document.querySelector(".increase");
 
 calculateData();
 
 function calculateData() {
   let totalSpendings = 0;
-  let highestDay = data[0];
+  let highestDay;
+  let highestBar;
 
   data.forEach((currentDay)=> {
     let amount = currentDay.amount;
     let day = currentDay.day;
+    
+    let barContainer = document.createElement("div");
+    barContainer.classList.add("bar-container")
+    
+    let hoverBar = document.createElement("div");
+    hoverBar.innerHTML = `<p>$${amount}</p>`;
+    hoverBar.classList.add("hover-bar");
+
+    let bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.style.height = `${amount * 2}px`;
+
+    barContainer.appendChild(hoverBar);
+    barContainer.appendChild(bar);
+    barContainer.insertAdjacentHTML("beforeend",`<p>${day}</p>`);
 
     totalSpendings += amount;
-    if (amount > highestDay.amount) {
-      highestDay = currentDay;
-    }
 
-    let newBar = ``
+    if (highestDay === undefined || highestDay.amount < amount) {
+      highestDay = currentDay;
+      highestBar = bar;
+    }
     
-    chart.insertAdjacentHTML("beforeend", newBar);
-    
+    chart.appendChild(barContainer);
   })
-  
+
+  highestBar.classList.add("highest-bar");
   totalAmount.innerHTML = `$${totalSpendings}`;
 }
+
+// 
