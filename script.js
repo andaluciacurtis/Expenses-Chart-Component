1f3af75b0b884003b4b0bd2a6c9ba759f040d2fd
andaluciacurtis <143487070+andaluciacurtis@users.noreply.github.com>
2023-11-28 21:18:32 +0000

    Adding JavaScript file

---
+++ script.js
@@ -0,0 +1,19 @@
+import file from "./data.json" assert { type: 'json'};
+
+const barContainers = document.querySelectorAll(".bar-container");
+
+const balance = document.querySelector(".balance");
+const total = document.querySelector(".total");
+const increase = document.querySelector(".increase");
+
+function calculateData() {
+  let total = 0;
+  let highestDay = 0;
+
+    // get data from json file
+
+  
+    // add total all together
+    // show the increase from last month
+    // show the highest day
+}
